2b7bb66a66d42a2f0b4ebe69e262a79abee4b40e
Shahad Hassan <shahadg1983@gmail.com>
2024-12-28 15:47:39 +0530

    feat: Slider added

--- app/components/ServicesSection4.jsx
+++ app/components/ServicesSection4.jsx
@@ -1,16 +1,45 @@
+"use client";
 import Image from "next/image";
 import Separator from "@/public/Separator.png";
 
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 export default function ServicesSection4() {
     return (
         <div className="min-h-[38rem] lg:px-[120px] w-full py-[90px] bg-[#111319] flex flex-col justify-center place-content-between">
             <div className="flex flex-col">
                 <p className="flex items-center text-base font-normal text-[#FFFFFF]"><Image src={Separator} className="mr-[10px]" alt="separator"/> Our Services</p>
-                <span className="mt-5 font-bold text-[64px] leading-[76.8px] text-[#FFFFFF]">What our Client&apos;s Say</span>
+                <span className="mt-5 font-bold text-[64px] leading-[76.8px] text-[#FFFFFF]">We are more than our words</span>
             </div>
-            <div className="flex flex-row justify-center place-content-between gap-x-10 mt-9">
-                
+            <div className="mt-9">
+                <Swiper
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="w-full text-white min-h-[387px] bg-[#191C26]"
+                >
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center">
+                        Slide 1
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center">
+                        Slide 2
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center">
+                        Slide 3
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </div>
     )
--- app/components/ServicesSection5.jsx
+++ app/components/ServicesSection5.jsx
@@ -9,7 +9,9 @@ export default function ServicesSection5() {
                 <p className="flex items-center text-base font-normal text-[#FFFFFF]"><Image src={Separator} className="mr-[10px]" alt="separator"/> Our Services</p>
                 <span className="mt-5 font-bold text-[64px] leading-[76.8px] text-[#FFFFFF]">Book a Discovery Session</span>
             </div>
-            <div className="flex flex-row justify-center place-content-between gap-x-10 mt-9">
+            <div className="w-full text-white flex place-content-between justify-center gap-8 mt-9">
+                <div className="bg-[#191C26] w-[501px] min-h-[387px]">Hi</div>
+                <div className="bg-[#191C26] w-[655px] min-h-[387px]">Hi</div>
             </div>
         </div>
     )
