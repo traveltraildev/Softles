8602ff970d4c79dc67e86db7ba951d256b103cea
Shahad Hassan <shahadg1983@gmail.com>
2024-12-28 15:53:58 +0530

    fix: Dependency conflict resolved

--- app/components/ServicesSection2.jsx
+++ app/components/ServicesSection2.jsx
@@ -7,7 +7,7 @@ export default function ServicesSection2() {
         <div className="min-h-[38rem] lg:px-[120px] w-full py-[90px] bg-[#111319] flex flex-col justify-center place-content-between">
             <div className="flex flex-col">
                 <p className="flex items-center text-base font-normal text-[#FFFFFF]"><Image src={Separator} className="mr-[10px]" alt="separator"/> Our Services</p>
-                <span className="mt-5 font-bold text-[64px] leading-[76.8px] text-[#FFFFFF]">What our Client&apos;s Say</span>
+                <span className="mt-5 font-bold text-[64px] leading-[76.8px] text-[#FFFFFF]">We are more than our words</span>
             </div>
             <div className="flex flex-row justify-center place-content-between gap-x-10 mt-9">
                 
